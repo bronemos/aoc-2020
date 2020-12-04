6810f2971f8075d0689edf27a84052321815849b
bronemos <spieglb@gmail.com>
2020-12-04 15:58:21 +0100

    updated day1 and day2

--- day3.py
+++ day3.py
@@ -1,8 +1,8 @@
 import sys
 
-#pt 1
+# pt1
 # print(''.join([y[i*3 if i*3 < len(y) else i*3 - (i*3)//len(y)*len(y)] for i, y in enumerate([x.strip() for x in sys.stdin.readlines()])]).count('#'))
 
-#pt 2
+# pt2
 
 print([x := (i+1)*num if i == 0 else x*num for i, num in enumerate([[''.join([y[i*r if i*r < len(y) else i*r - (i*r)//len(y)*len(y)] for i, y in enumerate((user_in[::d]))]).count('#') for r, d in ((1, 1), (3, 1), (5, 1), (7, 1), (1, 2))] for user_in in [[x.strip() for x in sys.stdin.readlines()]]][0])][-1])
