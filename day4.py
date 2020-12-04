84b355d8c01a05ce27d1db51426e93d4a549e0cc
bronemos <spieglb@gmail.com>
2020-12-04 17:53:07 +0100

    day4 sol

--- day4.py
+++ day4.py
@@ -6,9 +6,7 @@ import re
 # print(len([z for z in [re.split(':|\\s', y) for y in [x.replace('\n', ' ') for x in ''.join(sys.stdin.readlines()).split('\n'*2)]] if len({'byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'} - set(z)) == 0]))
 
 # pt2
-#print([print(x, y) for x, y in [zip(z[::2], z[1::2]) for z in [re.split(':|\\s', y) for y in [x.replace('\n', ' ') for x in ''.join(sys.stdin.readlines()).split('\n'*2)]] if len({'byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'} - set(z)) == 0]])
-re_dict = dict()
-print([z for z in [re.split(':|\\s', y) for y in [x.replace('\n', ' ') for x in ''.join(sys.stdin.readlines()).split('\n'*2)]] if len({'byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'} - set(z)) == 0])
-b = [[1, 'a', 2, 'b'], [3, 'c', 4, 'd']]
-print([zip(a[::2], a[1::2]) for a in b])
-[print(x, y, sep='\n') for x, y in [zip(a[::2], a[1::2]) for a in b]]
+# re_dict ={'byr': re.compile(r'(19[2-9][0-9]|200[0-2])'), 'iyr': re.compile(r'20(1[0-9]|20)'), 'eyr': re.compile(r'20(2[0-9]|30)'), 'hgt': re.compile(r'((1[5-8][0-9]|19[0-3])cm|(59|6[0-9]|7[0-6])in)'), 'hcl': re.compile(r'#[0-9a-f]{6}'), 'ecl': re.compile(r'(amb|blu|brn|gry|grn|hzl|oth)'), 'pid': re.compile(r'^\d{9,9}$'), 'cid': re.compile(r'.*')}
+
+print(len([passport for passport in [[(x, y) for x, y in zip(z[::2], z[1::2])] for z in [re.split(':|\\s', y) for y in [x.replace('\n', ' ') for x in ''.join(sys.stdin.readlines()).split('\n' * 2)]] if len({'byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'} - set(z)) == 0] if all([{'byr': re.compile(r'(19[2-9][0-9]|200[0-2])'), 'iyr': re.compile(r'20(1[0-9]|20)'), 'eyr': re.compile(r'20(2[0-9]|30)'), 'hgt': re.compile(r'((1[5-8][0-9]|19[0-3])cm|(59|6[0-9]|7[0-6])in)'), 'hcl': re.compile(r'#[0-9a-f]{6}'), 'ecl': re.compile(r'(amb|blu|brn|gry|grn|hzl|oth)'), 'pid': re.compile(r'^\d{9,9}$'), 'cid': re.compile(r'.*')}
+[data_type].match(data) for data_type, data in passport])]))
