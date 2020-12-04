899443f3a0de2d3d1b7869f864b9dee533cb9687
bronemos <spieglb@gmail.com>
2020-12-04 15:58:13 +0100

    updated day1 and day2

--- day4.py
+++ day4.py
@@ -1,10 +1,14 @@
 import sys
 import re
 
-#pt 1
+# pt1
 
-print(''.join(['a' if len({'byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'} - set(z)) == 0 else 'b' for z in [re.split(':|\\s', y)[::2] for y in [x.replace('\n', ' ') for x in ''.join(sys.stdin.readlines()).split('\n\n')]]]).count('a'))
+# print(len([z for z in [re.split(':|\\s', y) for y in [x.replace('\n', ' ') for x in ''.join(sys.stdin.readlines()).split('\n'*2)]] if len({'byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'} - set(z)) == 0]))
 
-#pt2
-
-
+# pt2
+#print([print(x, y) for x, y in [zip(z[::2], z[1::2]) for z in [re.split(':|\\s', y) for y in [x.replace('\n', ' ') for x in ''.join(sys.stdin.readlines()).split('\n'*2)]] if len({'byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'} - set(z)) == 0]])
+re_dict = dict()
+print([z for z in [re.split(':|\\s', y) for y in [x.replace('\n', ' ') for x in ''.join(sys.stdin.readlines()).split('\n'*2)]] if len({'byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'} - set(z)) == 0])
+b = [[1, 'a', 2, 'b'], [3, 'c', 4, 'd']]
+print([zip(a[::2], a[1::2]) for a in b])
+[print(x, y, sep='\n') for x, y in [zip(a[::2], a[1::2]) for a in b]]
