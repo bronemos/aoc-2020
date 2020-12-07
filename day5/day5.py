18881e3098a712f8e72d320e12c3d3d4a28afcfb
bronemos <spieglb@gmail.com>
2020-12-07 17:17:52 +0100

    day5 shorten

--- day5/day5.py
+++ day5/day5.py
@@ -1,7 +1,7 @@
 with open('input5.txt', 'r') as f:
     # pt1
 
-    print(max(seats := [([interval := (0, 63) if i == 0 and character == 'F' else (64, 127) if i == 0 and character == 'B' else (interval[0], interval[1] - (interval[1] - interval[0]) // 2 - 1) if character == 'F' else (interval[0] + (interval[1] - interval[0]) // 2 + 1, interval[1]) for i, character in enumerate(row)][-1][0 if row[-1] == 'F' else 1] * 8 + [interval := (0, 3) if i == 0 and character == 'L' else (4, 7) if i == 0 and character == 'R' else (interval[0], interval[1] - (interval[1] - interval[0]) // 2 - 1) if character == 'L' else (interval[0] + (interval[1] - interval[0]) // 2 + 1, interval[1]) for i, character in enumerate(column)][-1][0 if column[-1] == 'L' else 1]) for row, column in [(y[:7], y[7:]) for y in [x.strip() for x in f.readlines()]]]))
+    print(max(seats := [int(x.strip().replace('F', '0').replace('B', '1').replace('L', '0').replace('R', '1'), 2) for x in f.readlines()]))
 
     # pt 2
 
