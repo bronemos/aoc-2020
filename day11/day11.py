6c8f4e7d65564c6033f84163b3f85c2c1d176534
bronemos <spieglb@gmail.com>
2020-12-11 14:49:33 +0100

    day11 pt2

--- day11/day11.py
+++ day11/day11.py
@@ -31,6 +31,8 @@ with open('input11.txt', 'r') as f:
     f.seek(0)
     # pt2
 
+    directions = ((1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (1, -1), (-1, 1), (-1, -1))
+
     seats = [row.strip() for row in f.readlines()]
     while True:
         new_seats = []
@@ -40,19 +42,29 @@ with open('input11.txt', 'r') as f:
                 if seat == '.':
                     new_row += '.'
                 else:
-                    occupied_seats = ''.join(
-                        [row[j + 1] if j < len(row) - 1 else '',
-                         row[j - 1] if j > 0 else '',
-                         seats[i - 1][max(0, j - 1): min(len(row), j + 2)] if i > 0 else '',
-                         seats[i + 1][max(0, j - 1): min(len(row), j + 2)] if i < len(seats) - 1 else '']).count('#')
-                    if seat == '#' and occupied_seats >= 4:
+                    visible_seats = 0
+
+                    for x, y in directions:
+                        i_copy = i
+                        j_copy = j
+                        while True:
+                            j_copy += x
+                            i_copy += y
+                            if 0 <= i_copy < len(seats) and 0 <= j_copy < len(row):
+                                if seats[i_copy][j_copy] == '#':
+                                    visible_seats += 1
+                                    break
+                                elif seats[i_copy][j_copy] == 'L':
+                                    break
+                            else:
+                                break
+                    if seat == '#' and visible_seats >= 5:
                         new_row += 'L'
-                    elif seat == 'L' and occupied_seats == 0:
+                    elif seat == 'L' and visible_seats == 0:
                         new_row += '#'
                     else:
                         new_row += seat
             new_seats.append(new_row)
-
         if new_seats == seats:
             print(sum([row.count('#') for row in seats]))
             break
