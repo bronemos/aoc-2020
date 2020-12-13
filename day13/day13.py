4d5d256d0534da831526c3dd33d4de7578b368e6
bronemos <spieglb@gmail.com>
2020-12-13 15:39:02 +0100

    day13 pt2

--- day13/day13.py
+++ day13/day13.py
@@ -1,15 +1,26 @@
+from math import prod
+
 with open('input13.txt', 'r') as f:
     time, bus_nums = int(f.readline()), [int(x) for x in f.readline().split(',') if x != 'x']
 
 # pt1
 
-min_time, num = min([((time // num * num + num) % time, num) for num in bus_nums], key=lambda x: x[0])
-print(min_time * num)
+earliest_time, num = min([((time // num * num + num) % time, num) for num in bus_nums], key=lambda x: x[0])
+print(earliest_time * num)
+
 
 # pt2
 
+# chinese remainder theorem
+
+def chinese_remainder(n, a):
+    p = prod(n)
+    total = sum(y * pow(p // x, -1, x) * (p // x) for x, y in zip(n, a))
+    return total % p
+
+
 with open('input13.txt', 'r') as f:
-    _, bus_nums = int(f.readline()), [int(x) if x != 'x' else x for x in f.readline().split(',')]
+    _, bus_nums = int(f.readline()), [(int(x), int(x) - offset) for offset, x in enumerate(f.readline().split(',')) if
+                                      x != 'x']
 
-
-
+    print(chinese_remainder([x[0] for x in bus_nums], [x[1] for x in bus_nums]))
